8714dba6853ee423f63447c5dd1539d13d0a043a
Edgar Khimich <ek@brainfinance.com>
2021-11-30 23:39:59 -0500

    Textfields, Buttons & Showing Snackbars

--- app/src/main/java/com/example/composetestapplication/MainActivity.kt
+++ app/src/main/java/com/example/composetestapplication/MainActivity.kt
@@ -3,76 +3,80 @@ package com.example.composetestapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.MutableLiveData
-import kotlin.random.Random
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize()
+            val scaffoldState = rememberScaffoldState()
+
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+
+            val scope = lifecycleScope
+            val focusManager = LocalFocusManager.current
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
             ) {
-                val previousBoxColor = remember {
-                    MutableLiveData<Color>()
-                }
 
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-
-                ColorBox(
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
-                        .weight(1f)
-                        .fillMaxSize(),
-                    previousBoxColor = previousBoxColor.value,
-                    colorBoxOnClickListener = {
-                        previousBoxColor.value = color.value
-                        color.value = it
-                    }
-                )
-                Box(
-                    modifier = Modifier
-                        .background(color.value)
-                        .weight(1f)
                         .fillMaxSize()
-                )
+                        .padding(
+                            horizontal = 30.dp
+                        )
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text(
+                                text = "Enter your name"
+                            )
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                    Button(
+                        onClick = {
+                            focusManager.clearFocus()
+
+                            scope.launch {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Hello $textFieldState"
+                                )
+                            }
+                        }
+                    ) {
+                        Text(
+                            text = "Here we go"
+                        )
+                    }
+                }
             }
         }
     }
-}
-
-
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    previousBoxColor: Color?,
-    colorBoxOnClickListener: (Color) -> Unit
-) {
-    Box(
-        modifier = modifier
-            .background(previousBoxColor ?: Color.Black)
-            .clickable {
-                colorBoxOnClickListener(
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        1f
-                    )
-                )
-            }
-    )
-
 }
